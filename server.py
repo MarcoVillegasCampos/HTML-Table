c619f7549153e61e0ffdffbf449c0e8017da3724
Your Name <you@example.com>
2021-09-23 23:37:17 -0600

    files

--- server.py
+++ server.py
@@ -3,34 +3,17 @@ from flask import Flask, render_template
 
 app= Flask( __name__)
 
+users = [
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
 
 @app.route('/', methods=['GET'])
 def loadHome():
-    x=int(8)
-    y=int(8)
-    return render_template('index.html', x=x, y=y)
-
-
-@app.route('/play/<x>/<y>', methods=['GET'])
-def loadTimes(x,y):
-    x=int(x)
-    y=int(y)
-    return render_template('index.html', x=x, y=y)
-
-@app.route('/play/<y>', methods=['GET'])
-def loadOnce(y):
-    x=int(8)
-    y=int(y)
-    return render_template('index.html', x=x, y=y)
-
-@app.route('/play/<x>/<y>/<color1>/<color2>', methods=['GET'])
-def loadColor(x,y,color1,color2):
-    x=int(x)
-    y=int(y)
-    color1=color1
-    color2=color2
-    return render_template('index.html', x=x,y=y, color1=color1, color2=color2)
-
+   
+    return render_template('index.html', users=users)
 
 
 
